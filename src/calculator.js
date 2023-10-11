effb605fafffbcb03ae951c19b854e5193c46dd5
Zuravl. <dmitrijzuravlev36357@gmail.com>
2023-10-11 10:11:29 +0300

    Pabota

--- src/calculator.js
+++ src/calculator.js
@@ -1,14 +1,14 @@
 const multiplication = (x, y) => {
   let result = 0;
   // code
-
+  result =  x * y;
   //
   return result;
 }
 
 const calculateAverage = (value1, value2, value3, value4, value5) => {
   // code
-
+return ((value1, value2, value3, value4, value5) / 5);
   //
 }
 
--- src/divisibleAndEvenValue.js
+++ src/divisibleAndEvenValue.js
@@ -1,3 +1,5 @@
-const isDivisibleBy5AndEven = () => { };
+const isDivisibleBy5AndEven = (number) => {
+    return ((number % 5 === 0) && (number % 2 === 0));
+ };
 
 export { isDivisibleBy5AndEven };
